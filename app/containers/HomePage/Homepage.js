68f45e6266d1620b1b89989f35222e0ea8328266
Andres Fernando Saa <andressaa.dev@gmail.com>
2020-06-02 21:40:16 -0500

    Separando la estructura y los estilos de la lista de posts del homepage

--- app/containers/HomePage/Homepage.js
+++ app/containers/HomePage/Homepage.js
@@ -10,7 +10,7 @@ import './Homepage.scss';
 // import Header from '../../components/Header/Header.js';
 // import Footer from '../../components/Footer/Footer.js';
 import { Helmet } from 'react-helmet-async';
-import playIcon from '../../components/common/assets/images/play-icon.png';
+import PostsList from '../../components/PostsList/PostsList';
 
 export default class HomePage extends React.PureComponent {
   render() {
@@ -30,82 +30,7 @@ export default class HomePage extends React.PureComponent {
 
         <section className="carousel">
           <div className="carousel__container">
-            <div className="carousel-item">
-              <img
-                className="carousel-item__img"
-                src="https://images.unsplash.com/photo-1500099817043-86d46000d58f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"
-                alt="Person Drowning"
-              />
-              <div className="carousel-item__details">
-                <div>
-                  <img src={playIcon} alt="Play icon" />
-                </div>
-                <p className="carousel-item__details--title">
-                  Las deudas te ahogan?
-                </p>
-                <p className="carousel-item__details--subtitle">
-                  Los intereses que pagas en tus deudas cuentan y te pueden
-                  perjudicar.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                className="carousel-item__img"
-                src="https://images.unsplash.com/photo-1554224155-8d04cb21cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                alt="Budgeting"
-              />
-              <div className="carousel-item__details">
-                <div>
-                  <img src={playIcon} alt="Play icon" />
-                </div>
-                <p className="carousel-item__details--title">
-                  Aprende a crear tu presupuesto
-                </p>
-                <p className="carousel-item__details--subtitle">
-                  Determina en que estas gastando tu dinero y que cantidad.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                className="carousel-item__img"
-                src="https://images.unsplash.com/photo-1567427013953-88102117053a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                alt="Saving"
-              />
-              <div className="carousel-item__details">
-                <div>
-                  <img src={playIcon} alt="Play icon" />
-                </div>
-                <p className="carousel-item__details--title">
-                  Desarrolla el habito de ahorrar
-                </p>
-                <p className="carousel-item__details--subtitle">
-                  Ya sea para obtener lo que quieres o para alcanzar tus metas.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                className="carousel-item__img"
-                src="https://images.unsplash.com/photo-1556155092-8707de31f9c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                alt="Investing"
-              />
-              <div className="carousel-item__details">
-                <div>
-                  <img src={playIcon} alt="Play icon" />
-                </div>
-                <p className="carousel-item__details--title">
-                  Invierte como debe ser
-                </p>
-                <p className="carousel-item__details--subtitle">
-                  La mayoría de personas deberían invertir siempre a largo plazo
-                  y de manera consistente.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item"></div>
-            <div className="carousel-item"></div>
+            <PostsList />
           </div>
         </section>
       </div>
